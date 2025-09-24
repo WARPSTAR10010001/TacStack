71c776d2f0ba75535dd6ccb53773b217578ffd32
WARPSTAR <alpay.gueruez@hsrw.org>
2025-09-24 03:35:49 +0200

    #2

--- logic.js
+++ logic.js
@@ -2,24 +2,122 @@
 const overlay = document.getElementById("overlay-container");
 const game = document.getElementById("game-container");
 const settings = document.getElementById("settings-container");
-
-let field = {
-    x1y1: "",
-    x1y2: "",
-    x1y3: "",
-    x2y1: "",
-    x2y2: "",
-    x2y3: "",
-    x3y1: "",
-    x3y2: "",
-    x3y3: ""
+let gameInfo = {
+    aiEnemy: false,
+    aiLevel: 1,
+    startingPlayer: 1, // 1 is default, 0 is ai, 2 is other player
+    playerOneName: "Player 1",
+    playerTwoName: "Player 2",
+    firstTurn: true,
+    currentTurn: null
 };
+gameInfo.currentTurn = gameInfo.startingPlayer;
+let field = {
+    11: null,
+    12: null,
+    13: null,
+    21: null,
+    22: null,
+    23: null,
+    31: null,
+    32: null,
+    33: null
+}; // use this to make system more robust against cheats
+
+for (let i = 1; i <= 3; i++) {
+    for (let j = 1; j <= 3; j++) {
+        let id = i * 10 + j;
+        document.getElementById(id).addEventListener(("click"), claimBox(id, gameInfo.currentTurn));
+    }
+}
 
 showOverlay("welcome", "Welcome to TacStack", "TicTacToe but reimagined as a web-based game!", "Let's have fun!");
 
 document.getElementById("settings-button").addEventListener(("click"), () => {
     settings.style.display = "none";
-    showOverlay("start", "Start Game", "Click one of the buttons below to start a new game or change the current settings!", "Play");
+    showOverlay("start", "Start a new game", "Click one of the buttons below to start a new game or change the current settings!", "Play TacStack");
+})
+
+document.getElementById("playWithAi-btn").addEventListener(("click"), () => {
+    if (gameInfo.aiEnemy === true) {
+        document.getElementById("playWithAi-btn").innerText = "Off";
+        gameInfo.aiEnemy = false;
+        document.getElementById("settings-aiLevel").style.display = "none";
+        document.getElementById("settings-playerTwoName").style.display = "block";
+        document.getElementById("startingPlayer-btn").innerText = gameInfo.playerOneName;
+        gameInfo.startingPlayer = 1;
+    } else {
+        document.getElementById("playWithAi-btn").innerText = "On";
+        gameInfo.aiEnemy = true;
+        document.getElementById("settings-aiLevel").style.display = "block";
+        document.getElementById("settings-playerTwoName").style.display = "none";
+        document.getElementById("startingPlayer-btn").innerText = gameInfo.playerOneName;
+        gameInfo.startingPlayer = 1;
+    }
+})
+
+document.getElementById("aiLevel-btn").addEventListener(("click"), () => {
+    if (gameInfo.aiLevel === 1) {
+        document.getElementById("aiLevel-btn").innerText = "Medium";
+        gameInfo.aiLevel = 2;
+    } else if (gameInfo.aiLevel === 2) {
+        document.getElementById("aiLevel-btn").innerText = "Hard";
+        gameInfo.aiLevel = 3;
+    } else {
+        document.getElementById("aiLevel-btn").innerText = "Easy";
+        gameInfo.aiLevel = 1;
+    }
+})
+
+document.getElementById("startingPlayer-btn").addEventListener(("click"), () => {
+    if (gameInfo.startingPlayer === 1) {
+        if (gameInfo.aiEnemy === true) {
+            document.getElementById("startingPlayer-btn").innerText = "AI";
+            gameInfo.startingPlayer = 0;
+        } else {
+            document.getElementById("startingPlayer-btn").innerText = gameInfo.playerTwoName;
+            gameInfo.startingPlayer = 2;
+        }
+    } else {
+        document.getElementById("startingPlayer-btn").innerText = gameInfo.playerOneName;
+        gameInfo.startingPlayer = 1;
+    }
+})
+
+document.getElementById("playerOneName-input").addEventListener(("input"), (e) => {
+    if (gameInfo.startingPlayer === 1) {
+        if (e.target.value.length === 0 || e.target.value.length > 20) {
+            gameInfo.playerOneName = "Player 1";
+            document.getElementById("startingPlayer-btn").innerText = gameInfo.playerOneName;
+        } else {
+            gameInfo.playerOneName = e.target.value;
+            document.getElementById("startingPlayer-btn").innerText = gameInfo.playerOneName;
+        }
+    } else {
+        if (e.target.value.length === 0 || e.target.value.length > 20) {
+            gameInfo.playerOneName = "Player 1";
+        } else {
+            gameInfo.playerOneName = e.target.value;
+        }
+    }
+})
+
+document.getElementById("playerTwoName-input").addEventListener(("input"), (e) => {
+    if (gameInfo.startingPlayer === 2) {
+        if (e.target.value.length === 0 || e.target.value.length > 20) {
+            gameInfo.playerTwoName = "Player 2";
+            document.getElementById("startingPlayer-btn").innerText = gameInfo.playerTwoName;
+        } else {
+            gameInfo.playerTwoName = e.target.value;
+            document.getElementById("startingPlayer-btn").innerText = gameInfo.playerTwoName;
+        }
+    } else {
+        if (e.target.value.length === 0 || e.target.value.length > 20) {
+            gameInfo.playerTwoName = "Player 2";
+        } else {
+            gameInfo.playerTwoName = e.target.value;
+        }
+    }
 })
 
 function showOverlay(state, title, msg, btn) {
@@ -39,9 +137,9 @@ function showOverlay(state, title, msg, btn) {
     document.getElementById("overlay-button-primary").addEventListener(("click"), () => {
         if (state === "start") {
             overlay.style.display = "none";
-            game.style.display = "block";
+            game.style.display = "flex";
         } else if (state === "end" || state === "welcome") {
-            showOverlay("start", "Start Game", "Click one of the buttons below to start a new game or change the current settings!", "Play");
+            showOverlay("start", "Start a new game", "Click one of the buttons below to start a new game or change the current settings!", "Play TacStack");
         } else {
             overlay.style.display = "none";
         }
@@ -52,4 +150,32 @@ function showOverlay(state, title, msg, btn) {
             settings.style.display = "block";
         }
     })
+}
+
+function claimBox(id, player) {
+    if (gameInfo.firstTurn) {
+        gameInfo.firstTurn = false;
+        if (player === 1) {
+            document.getElementById(id).innerText = "X";
+        } else {
+            document.getElementById(id).innerText = "O";
+        }
+    } else {
+        if (document.getElementById(id).innerText !== "") {
+            if (player === 1) {
+                document.getElementById(id).innerText = "X";
+            } else {
+                document.getElementById(id).innerText = "O";
+            }
+        }
+    }
+}
+
+function checkWin(player) {}
+
+function idToPos(id) {
+    return {
+        x: id % 10,
+        y: Math.trunc(id / 10)
+    };
 }
